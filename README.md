485f9ae3ef70cb9571f3401703dbab862859e45e
antonum <aumnikov@gmail.com>
2025-03-31 11:43:08 -0400

    add examples, remove infinite loop

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # mcp-server-bash
-minimalistic MCP server written in bash script
+
+Minimalistic MCP server written in bash script.
+
+JSON Configuration file for mcphost and Claude Desktop:
+
+```json
+{
+    "mcpServers": {
+      "math": {
+        "command": "/Users/anton/code/mcp-server-bash/mcp_add.sh",
+        "args": []
+      }
+    }
+  }
+```
+
+Test with mcphost
+
+```bash
+~/go/bin/mcphost -m ollama:llama3.1:latest --config /Users/anton/Timescale/mcp-server-bash/mcp.json
+
+...
+
+using math tool add 10 and 88
+```
+
+## References
+- Model Context Protocol https://www.anthropic.com/news/model-context-protocol
+- mcphost https://github.com/mark3labs/mcphos
--- mcp_add.sh
+++ mcp_add.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
+echo "Starting mcp_add.sh">> /Users/anton/code/mcp-server-bash/requests.log
 
-while true; do
-    while read -r line; do
-        # Parse JSON input using jq
-        method=$(echo "$line" | jq -r '.method' 2>/dev/null)
-        id=$(echo "$line" | jq -r '.id' 2>/dev/null)
-        num1=$(echo "$line" | jq -r '.num1' 2>/dev/null)
-        num2=$(echo "$line" | jq -r '.num2' 2>/dev/null)
-        if [[ "$method" == "initialize" ]]; then
-            #echo '{"jsonrpc":"2.0", "id": "'"$id"'"}'
-            echo '{"jsonrpc":"2.0","id":"'"$id"'","result":{"protocolVersion":"2024-11-05","capabilities":{"experimental":{},"prompts":{"listChanged":false},"resources":{"subscribe":false,"listChanged":false},"tools":{"listChanged":false}},"serverInfo":{"name":"math","version":"0.0.1"}}}'
-        elif [[ "$method" == "notifications/initialized" ]]; then
-            id=$id
-            #echo '{"jsonrpc":"2.0","status": "success", "result": $sum}"'
-        
-        elif [[ "$method" == "tools/list" ]]; then
-            echo '{"jsonrpc":"2.0","id":"'"$id"'","result":{"tools":[{"name":"addition","description":"addition of two numbers.\n\nArgs:\n    num1, num2\n","inputSchema":{"properties":{"num1":{"title":"Number1","type":"string"},"num2":{"title":"Number2","type":"string"}},"required":["num1", "num2"],"title":"get_alertsArguments","type":"object"}}]}}'
-        elif [[ "$method" == "resources/list" ]]; then
-            echo '{"jsonrpc":"2.0","id":'"$id"',"result":{"resources":[]}}'
+while read -r line; do
+    echo $line >> /Users/anton/code/mcp-server-bash/requests.log
+    # Parse JSON input using jq
+    method=$(echo "$line" | jq -r '.method' 2>/dev/null)
+    id=$(echo "$line" | jq -r '.id' 2>/dev/null)
+    if [[ "$method" == "initialize" ]]; then
+        #echo '{"jsonrpc":"2.0", "id": "'"$id"'"}'
+        echo '{"jsonrpc":"2.0","id":'"$id"',"result":{"protocolVersion":"2024-11-05","capabilities":{"experimental":{},"prompts":{"listChanged":false},"resources":{"subscribe":false,"listChanged":false},"tools":{"listChanged":false}},"serverInfo":{"name":"math","version":"0.0.1"}}}'
+    
+    elif [[ "$method" == "notifications/initialized" ]]; then
+        : #do nothing
+    
+    elif [[ "$method" == "tools/list" ]]; then
+        echo '{"jsonrpc":"2.0","id":'"$id"',"result":{"tools":[{"name":"addition","description":"addition of two numbers.\n\nArgs:\n    num1, num2\n","inputSchema":{"properties":{"num1":{"title":"Number1","type":"string"},"num2":{"title":"Number2","type":"string"}},"required":["num1", "num2"]}}]}}'
+    
+    elif [[ "$method" == "resources/list" ]]; then
+        echo '{"jsonrpc":"2.0","id":'"$id"',"result":{"resources":[]}}'
 
-        elif [[ "$method" == "prompts/list" ]]; then
-            echo '{"jsonrpc":"2.0","id":'"$id"',"result":{"prompts":[]}}'
+    elif [[ "$method" == "prompts/list" ]]; then
+        echo '{"jsonrpc":"2.0","id":'"$id"',"result":{"prompts":[]}}'
 
-        elif [[ "$method" == "tools/call" ]]; then
-            #{"method":"tools/call","params":{"name":"addition","arguments":{"num1":"1","num2":"2"}},"jsonrpc":"2.0","id":20}
-            tool_method=$(echo "$line" | jq -r '.params.name' 2>/dev/null)
-            tool_arguments=$(echo "$line" | jq -r '.params.arguments' 2>/dev/null)
-            num1=$(echo "$line" | jq -r '.params.arguments.num1' 2>/dev/null)
-            num2=$(echo "$line" | jq -r '.params.arguments.num2' 2>/dev/null)
-            sum=$((num1 + num2))
-            echo '{"jsonrpc":"2.0","id":'"$id"',"result":{"content":[{"type":"text","text":"\n sum of two numbers is '"$sum"'"}],"isError":false}}'
-        else
-            echo '{"jsonrpc":"2.0","status": "error", "message": "Invalid Input"}'
-        fi
-    done
-done
+    elif [[ "$method" == "tools/call" ]]; then
+        #{"method":"tools/call","params":{"name":"addition","arguments":{"num1":"1","num2":"2"}},"jsonrpc":"2.0","id":20}
+        tool_method=$(echo "$line" | jq -r '.params.name' 2>/dev/null)
+        tool_arguments=$(echo "$line" | jq -r '.params.arguments' 2>/dev/null)
+        num1=$(echo "$line" | jq -r '.params.arguments.num1' 2>/dev/null)
+        num2=$(echo "$line" | jq -r '.params.arguments.num2' 2>/dev/null)
+        sum=$((num1 + num2))
+        echo '{"jsonrpc":"2.0","id":'"$id"',"result":{"content":[{"type":"text","text":"\n sum of two numbers is '"$sum"'"}],"isError":false}}'
+    
+    else
+        echo '{"jsonrpc":"2.0","id":'"$id"',"error":{"code":-32601,"message":"Method not found"}}'
+    fi
+done || break
